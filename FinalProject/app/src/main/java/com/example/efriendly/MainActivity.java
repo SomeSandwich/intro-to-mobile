58d9332022b78825fb36bd893ea0552bc6df48c8
baokyo002 <74838783+baokyo002@users.noreply.github.com>
2023-03-20 21:42:36 +0700

    init and login layout

---
+++ FinalProject/app/src/main/java/com/example/efriendly/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.efriendly;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) { //Disable keyboard when click around
+        View view = getCurrentFocus();
+        if (view != null && (ev.getAction() == MotionEvent.ACTION_UP || ev.getAction() == MotionEvent.ACTION_MOVE) && view instanceof EditText && !view.getClass().getName().startsWith("android.webkit.")) {
+            int[] scrcoords = new int[2];
+            view.getLocationOnScreen(scrcoords);
+            float x = ev.getRawX() + view.getLeft() - scrcoords[0];
+            float y = ev.getRawY() + view.getTop() - scrcoords[1];
+            if (x < view.getLeft() || x > view.getRight() || y < view.getTop() || y > view.getBottom())
+                ((InputMethodManager)this.getSystemService(Context.INPUT_METHOD_SERVICE)).hideSoftInputFromWindow((this.getWindow().getDecorView().getApplicationWindowToken()), 0);
+        }
+        return super.dispatchTouchEvent(ev);
+    }
+    
+    @SuppressLint("AppCompatMethod")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Hide Title
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+
+        setContentView(R.layout.activity_main);
+
+        //addEventClick
+        TextView forgotPass = (TextView) findViewById(R.id.forgotPass);
+        System.out.println(forgotPass);
+        if (forgotPass != null) forgotPass.setOnClickListener(OnClickForgotPass);
+    }
+
+    final private TextView.OnClickListener OnClickForgotPass = new TextView.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            System.out.println("Forgot password clicked");
+        }
+    };
+}
