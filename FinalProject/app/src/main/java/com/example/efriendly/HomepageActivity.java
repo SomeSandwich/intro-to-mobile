11d761041bef2199160a7893f1a8b6a15b5c85eb
baokyo002 <74838783+baokyo002@users.noreply.github.com>
2023-04-01 21:45:18 +0700

    Updata folder res

--- FinalProject/app/src/main/java/com/example/efriendly/HomepageActivity.java
+++ FinalProject/app/src/main/java/com/example/efriendly/HomepageActivity.java
@@ -3,8 +3,11 @@ package com.example.efriendly;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.efriendly.databinding.ActivityHomepageBinding;
+
 
 public class HomepageActivity extends AppCompatActivity {
+    private ActivityHomepageBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
