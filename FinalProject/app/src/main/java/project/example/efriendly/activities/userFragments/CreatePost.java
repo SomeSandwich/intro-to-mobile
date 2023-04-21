da80f7df05d92cacbcda11fef721a2f380a6765f
baokyo002 <74838783+baokyo002@users.noreply.github.com>
2023-04-21 21:58:02 +0700

    Fragment Addpost

---
+++ FinalProject/app/src/main/java/project/example/efriendly/activities/userFragments/CreatePost.java
@@ -0,0 +1,38 @@
+package project.example.efriendly.activities.userFragments;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import project.example.efriendly.R;
+import project.example.efriendly.activities.UserActivity;
+
+public class CreatePost extends Fragment {
+    UserActivity main;
+
+    Context context = null;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try{
+            context = getActivity();
+            main = (UserActivity) getActivity();
+        }
+        catch (IllegalStateException err){
+            throw new IllegalStateException("MainActivity must implement callbacks");
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create_post, container, false);
+    }
+}
