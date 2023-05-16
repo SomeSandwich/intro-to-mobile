bd94c0837bbd6906331cfc8f3ec83cae4972268d
Trương Trọng Khánh <82425500+trongkhanh11@users.noreply.github.com>
2023-05-16 17:56:10 +0700

    add logout + load user information

--- FinalProject/app/src/main/java/project/example/efriendly/activities/userFragments/ProfileActivity.java
+++ FinalProject/app/src/main/java/project/example/efriendly/activities/userFragments/ProfileActivity.java
@@ -1,35 +1,122 @@
 package project.example.efriendly.activities.userFragments;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.databinding.DataBindingUtil;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
-import android.view.WindowManager;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.AdapterView;
-import android.widget.EditText;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
 
 import project.example.efriendly.R;
-import project.example.efriendly.adapter.NotificationsAdapter;
-import project.example.efriendly.databinding.ActivityNotificationsBinding;
+import project.example.efriendly.activities.AnonymousHomepageActivity;
+import project.example.efriendly.activities.LoginActivity;
+import project.example.efriendly.activities.UserActivity;
+import project.example.efriendly.client.RetrofitClientGenerator;
+import project.example.efriendly.constants.DatabaseConnection;
+import project.example.efriendly.constants.StorageHelper;
+import project.example.efriendly.data.model.Auth.LoginReq;
+import project.example.efriendly.data.model.Auth.LoginRes;
+import project.example.efriendly.data.model.User.UserRes;
 import project.example.efriendly.databinding.ActivityProfileBinding;
+import project.example.efriendly.services.AuthService;
+import project.example.efriendly.services.UserService;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
-public class ProfileActivity extends Fragment {
+public class ProfileActivity extends Fragment implements DatabaseConnection {
+    UserActivity main;
+    Context context = null;
     private ActivityProfileBinding binding;
+    private UserService userService;
+    public ProfileActivity() {}
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try{
+            context = getActivity();
+            main = (UserActivity) getActivity();
+        }
+        catch (IllegalStateException err){
+            throw new IllegalStateException("MainActivity must implement callbacks");
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
         binding = ActivityProfileBinding.inflate(inflater, container,false);
+        binding.setClickHandler(new ProfileActivityClickHandler(context));
+
+        userService = RetrofitClientGenerator.getService(UserService.class);
+        Call<UserRes> userResCall = userService.GetSelf();
+        userResCall.enqueue(new Callback<UserRes>() {
+            @Override
+            public void onResponse(Call<UserRes> call, Response<UserRes> response) {
+                if (response.isSuccessful()) {
+                    UserRes userRes = response.body();
+                    binding.txtUsername.setText(userRes.getName());
+                    binding.txtEmail.setText(userRes.getEmail());
+                    binding.txtAddress.setText(userRes.getAddress());
+                    if (userRes.getAvatarPath() != null) {
+                        Glide.with(getActivity())
+                                .load(IMAGE_URL + userRes.getAvatarPath())
+                                .placeholder(R.drawable.placeholder)
+                                .into(binding.userAvatar);
+                    }
+                    else{
+                        binding.userAvatar.setImageResource(R.drawable.user);
+                    }
+                } else {
+                    String message = "An error occurred ...";
+                    Toast.makeText(getActivity(), message, Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<UserRes> call, Throwable t) {
+                String message = t.getLocalizedMessage();
+                Toast.makeText(getActivity(), message, Toast.LENGTH_LONG).show();
+            }
+        });
 
         return binding.getRoot();
+    }
+
+    public class ProfileActivityClickHandler {
+        Context context;
+
+        public ProfileActivityClickHandler(Context context) {this.context = context;}
+
+        public void logout(View view){
+            logoutMenu(ProfileActivity.this);
+        }
+
+        private void logoutMenu(ProfileActivity profileActivity) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(profileActivity.context);
+            builder.setTitle("Logout");
+            builder.setMessage("Are you sure you want to logout?");
+            builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    StorageHelper.Token = null;
+                    startActivity(new Intent(getActivity(), AnonymousHomepageActivity.class));
+                }
+            });
+            builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            });
+            builder.show();
+        }
     }
 }
--- FinalProject/app/src/main/java/project/example/efriendly/adapter/CartAdapter.java
+++ FinalProject/app/src/main/java/project/example/efriendly/adapter/CartAdapter.java
@@ -43,168 +43,170 @@ import project.example.efriendly.activities.userFragments.CartActivity;
 import project.example.efriendly.activities.userFragments.ChatActivity;
 import project.example.efriendly.data.model.User.UserRes;
 
-public class CartAdapter extends RecyclerView.Adapter<CartAdapter.ViewHolder> {
-    private ArrayList<CartItem> cartItemArrayList;
-    private CartManagement cartManagement;
-    private ChangeNumberItemsListener changeNumberItemsListener;
+public class CartAdapter extends RecyclerView.Adapter<CartHolder> implements DatabaseConnection {
+    private LayoutInflater inflater;
+    Context context;
+    List<CartRes> carts;
 
-    public CartAdapter(ArrayList<CartItem> cartItemArrayList, Context context, ChangeNumberItemsListener changeNumberItemsListener) {
-        this.cartItemArrayList = cartItemArrayList;
-        this.cartManagement = new CartManagement(context);
-        this.changeNumberItemsListener = changeNumberItemsListener;
+    UserService userService;
+    PostService postService;
+
+    CartActivity.ClickListener listener;
+
+    public CartAdapter( Context context, List<CartRes> carts,  CartActivity.ClickListener listener) {
+        this.context = context;
+        this.carts = carts;
+        this.listener = listener;
+    }
+
+    @NonNull
+    @Override
+    public CartHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        CustomCartItemsBinding binding = CustomCartItemsBinding.inflate(inflater, parent, false);
+        CartHolder cartHolder = new CartHolder(binding);
+        return cartHolder;
     }
 
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View inflate = LayoutInflater.from(parent.getContext()).inflate(R.layout.custom_cart_items, parent, false);
-        return new ViewHolder(inflate);
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull CartAdapter.ViewHolder holder, int position) {
-        holder.productName.setText(cartItemArrayList.get(position).getProductName());
-        holder.unitPrice.setText(String.valueOf(cartItemArrayList.get(position).getUnitPrice()));
-        holder.totalMoney.setText(String.valueOf(cartItemArrayList.get(position).getNumInCart() * cartItemArrayList.get(position).getUnitPrice()));
-        holder.numInCart.setText(String.valueOf(cartItemArrayList.get(position).getNumInCart()));
-
-        int drawableResourceId = holder.itemView.getContext().getResources().getIdentifier(cartItemArrayList.get(position).getMediaPath(), "drawable",
-                holder.itemView.getContext().getPackageName());
-
-        Glide.with(holder.itemView.getContext())
-                .load(drawableResourceId)
-                .into(holder.ivProduct);
-
-        holder.btnPlus.setOnClickListener(new View.OnClickListener() {
+    public void onBindViewHolder(@NonNull CartHolder holder, int position) {
+        final int index = holder.getAdapterPosition();
+        postService = RetrofitClientGenerator.getService(PostService.class);
+        userService = RetrofitClientGenerator.getService(UserService.class);
+        holder.checkBox.setChecked(listener.checkList.get(index).equals(Boolean.TRUE));
+        holder.checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onClick(View view) {
-                cartManagement.plusNumberProduct(cartItemArrayList, position, new ChangeNumberItemsListener() {
-                    @Override
-                    public void changed() {
-                        notifyDataSetChanged();
-                        changeNumberItemsListener.changed();
-                    }
-                });
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                listener.checkList.set(index, !listener.checkList.get(index));
             }
         });
 
-        holder.btnMinus.setOnClickListener(new View.OnClickListener() {
+        int PostId = carts.get(index).getPostId();
+        postService.GetById(PostId).enqueue(new Callback<PostRes>() {
             @Override
-            public void onClick(View view) {
-                cartManagement.minusNumberProduct(cartItemArrayList, position, new ChangeNumberItemsListener() {
-                    @Override
-                    public void changed() {
-                        notifyDataSetChanged();
-                        changeNumberItemsListener.changed();
-                    }
-                });
+            public void onResponse(Call<PostRes> call, Response<PostRes> response) {
+                if(response.isSuccessful()){
+                    PostRes post = response.body();
+                    Glide.with(context)
+                            .load(IMAGE_URL + post.getMediaPath().get(0)).placeholder(R.drawable.placeholder)
+                            .into(holder.productImg);
+                    holder.txtProductName.setText(post.getCaption());
+                    holder.txtPrice.setText(post.getPrice().toString());
+                    userService.GetById(post.getUserID()).enqueue(new Callback<UserRes>() {
+                        @Override
+                        public void onResponse(Call<UserRes> call, Response<UserRes> response) {
+                            if (response.isSuccessful()){
+                                holder.txtSeller.setText(response.body().getName());
+                            }
+                            else{
+                                String message = "An error occurred please try again later ...";
+                                Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+                            }
+                        }
+                        @Override
+                        public void onFailure(Call<UserRes> call, Throwable t) {
+                            String message = "An error occurred please try again later ...";
+                            Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }
+                else{
+                    String message = "An error occurred please try again later ...";
+                    Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+                }
+            }
+            @Override
+            public void onFailure(Call<PostRes> call, Throwable t) {
+                String message = "An error occurred please try again later ...";
+                Toast.makeText(context, message, Toast.LENGTH_LONG).show();
             }
         });
     }
 
     @Override
     public int getItemCount() {
-        return cartItemArrayList.size();
+        return carts.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
-        TextView productName, unitPrice, numInCart, totalMoney;
-        ImageView ivProduct, btnPlus, btnMinus;
-
-        public ViewHolder(@NonNull View itemView) {
-            super(itemView);
-            productName = itemView.findViewById(R.id.txtProductName);
-            unitPrice = itemView.findViewById(R.id.unitPrice);
-            ivProduct = itemView.findViewById(R.id.ivProduct);
-            totalMoney = itemView.findViewById(R.id.txtTotalMoney);
-            numInCart = itemView.findViewById(R.id.txtNumItems);
-            btnPlus = itemView.findViewById(R.id.btnPlusCart);
-            btnMinus = itemView.findViewById(R.id.btnMinusCart);
-        }
-    }
 }
 
-//public class CartAdapter extends RecyclerView.Adapter<CartHolder> implements DatabaseConnection {
-//    private LayoutInflater inflater;
-//    Context context;
-//    List<CartRes> carts;
-//    UserService userService;
-//    PostService postService;
-//    CartActivity.ClickListener listener;
+//public class CartAdapter extends RecyclerView.Adapter<CartAdapter.ViewHolder> {
+//    private ArrayList<CartItem> cartItemArrayList;
+//    private CartManagement cartManagement;
+//    private ChangeNumberItemsListener changeNumberItemsListener;
 //
-//    public CartAdapter( Context context, List<CartRes> carts,  CartActivity.ClickListener listener) {
-//        this.context = context;
-//        this.carts = carts;
-//        this.listener = listener;
-//    }
-//
-//    @NonNull
-//    @Override
-//    public CartHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-//        Context context = parent.getContext();
-//        LayoutInflater inflater = LayoutInflater.from(context);
-//        CustomCartItemsBinding binding = CustomCartItemsBinding.inflate(inflater, parent, false);
-//        CartHolder cartHolder = new CartHolder(binding);
-//        return cartHolder;
+//    public CartAdapter(ArrayList<CartItem> cartItemArrayList, Context context, ChangeNumberItemsListener changeNumberItemsListener) {
+//        this.cartItemArrayList = cartItemArrayList;
+//        this.cartManagement = new CartManagement(context);
+//        this.changeNumberItemsListener = changeNumberItemsListener;
 //    }
 //
 //    @Override
-//    public void onBindViewHolder(@NonNull CartHolder holder, int position) {
-//        final int index = holder.getAdapterPosition();
-//        postService = RetrofitClientGenerator.getService(PostService.class);
-//        userService = RetrofitClientGenerator.getService(UserService.class);
-//        holder.checkBox.setChecked(listener.checkList.get(index).equals(Boolean.TRUE));
-//        holder.checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+//        View inflate = LayoutInflater.from(parent.getContext()).inflate(R.layout.custom_cart_items, parent, false);
+//        return new ViewHolder(inflate);
+//    }
+//
+//    @Override
+//    public void onBindViewHolder(@NonNull CartAdapter.ViewHolder holder, int position) {
+//        holder.productName.setText(cartItemArrayList.get(position).getProductName());
+//        holder.unitPrice.setText(String.valueOf(cartItemArrayList.get(position).getUnitPrice()));
+//        holder.totalMoney.setText(String.valueOf(cartItemArrayList.get(position).getNumInCart() * cartItemArrayList.get(position).getUnitPrice()));
+//        holder.numInCart.setText(String.valueOf(cartItemArrayList.get(position).getNumInCart()));
+//
+//        int drawableResourceId = holder.itemView.getContext().getResources().getIdentifier(cartItemArrayList.get(position).getMediaPath(), "drawable",
+//                holder.itemView.getContext().getPackageName());
+//
+//        Glide.with(holder.itemView.getContext())
+//                .load(drawableResourceId)
+//                .into(holder.ivProduct);
+//
+//        holder.btnPlus.setOnClickListener(new View.OnClickListener() {
 //            @Override
-//            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-//                listener.checkList.set(index, !listener.checkList.get(index));
+//            public void onClick(View view) {
+//                cartManagement.plusNumberProduct(cartItemArrayList, position, new ChangeNumberItemsListener() {
+//                    @Override
+//                    public void changed() {
+//                        notifyDataSetChanged();
+//                        changeNumberItemsListener.changed();
+//                    }
+//                });
 //            }
 //        });
 //
-//        int PostId = carts.get(index).getPostId();
-//        postService.GetById(PostId).enqueue(new Callback<PostRes>() {
+//        holder.btnMinus.setOnClickListener(new View.OnClickListener() {
 //            @Override
-//            public void onResponse(Call<PostRes> call, Response<PostRes> response) {
-//                if(response.isSuccessful()){
-//                    PostRes post = response.body();
-//                    Glide.with(context)
-//                            .load(IMAGE_URL + post.getMediaPath().get(0)).placeholder(R.drawable.placeholder)
-//                            .into(holder.productImg);
-//                    holder.txtProductName.setText(post.getCaption());
-//                    holder.txtPrice.setText(post.getPrice().toString());
-//                    userService.GetById(post.getUserID()).enqueue(new Callback<UserRes>() {
-//                        @Override
-//                        public void onResponse(Call<UserRes> call, Response<UserRes> response) {
-//                            if (response.isSuccessful()){
-//                                holder.txtSeller.setText(response.body().getName());
-//                            }
-//                            else{
-//                                String message = "An error occurred please try again later ...";
-//                                Toast.makeText(context, message, Toast.LENGTH_LONG).show();
-//                            }
-//                        }
-//                        @Override
-//                        public void onFailure(Call<UserRes> call, Throwable t) {
-//                            String message = "An error occurred please try again later ...";
-//                            Toast.makeText(context, message, Toast.LENGTH_LONG).show();
-//                        }
-//                    });
-//                }
-//                else{
-//                    String message = "An error occurred please try again later ...";
-//                    Toast.makeText(context, message, Toast.LENGTH_LONG).show();
-//                }
-//            }
-//            @Override
-//            public void onFailure(Call<PostRes> call, Throwable t) {
-//                String message = "An error occurred please try again later ...";
-//                Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+//            public void onClick(View view) {
+//                cartManagement.minusNumberProduct(cartItemArrayList, position, new ChangeNumberItemsListener() {
+//                    @Override
+//                    public void changed() {
+//                        notifyDataSetChanged();
+//                        changeNumberItemsListener.changed();
+//                    }
+//                });
 //            }
 //        });
 //    }
 //
 //    @Override
 //    public int getItemCount() {
-//        return carts.size();
+//        return cartItemArrayList.size();
 //    }
 //
+//    public class ViewHolder extends RecyclerView.ViewHolder {
+//        TextView productName, unitPrice, numInCart, totalMoney;
+//        ImageView ivProduct, btnPlus, btnMinus;
+//
+//        public ViewHolder(@NonNull View itemView) {
+//            super(itemView);
+//            productName = itemView.findViewById(R.id.txtProductName);
+//            unitPrice = itemView.findViewById(R.id.unitPrice);
+//            ivProduct = itemView.findViewById(R.id.ivProduct);
+//            totalMoney = itemView.findViewById(R.id.txtTotalMoney);
+//            numInCart = itemView.findViewById(R.id.txtNumItems);
+//            btnPlus = itemView.findViewById(R.id.btnPlusCart);
+//            btnMinus = itemView.findViewById(R.id.btnMinusCart);
+//        }
+//    }
 //}
